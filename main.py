ed6f3c419aad18e832982da5f8f6aa9d5aa03a24
Ethan Rylko <ethan.rylko@gmail.com>
2024-03-13 12:53:57 -0400

    Documentation and cleanup

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-from bs4 import BeautifulSoup
+from typing import *
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
-import pyautogui
+from selenium.webdriver.remote.webelement import WebElement
 
 driver_path = 'C:\\Users\\egame\\chromedriver'
 options = Options()
-# options.add_argument('--headless')
-# options.add_argument('user-agent=Chrome/58.0.3029.110')
 driver = webdriver.Chrome(options=options)
 
 start_button_class = 'Feo8La_playButton'
@@ -17,7 +15,16 @@ close_button_class = 'PwGt5a_closeX'
 hint_div_class = 'XmXXwG_hint'
 board_class = 'UOpmtW_board'
 
+# I hope these stay cosntant
+ROWS = 8
+COLS = 6
+
+
+
 def start():
+    """
+    Click through some buttons to get to the puzzle
+    """
     print('Hello World!')
 
     driver.get('https://www.nytimes.com/games/strands')
@@ -27,7 +34,14 @@ def start():
     close_button = WebDriverWait(driver, 2).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, close_button_class)))
     close_button.click()
 
+
 def find_total_words():
+    """
+    Find the total number of words in the puzzle
+
+    Returns:
+        int: Number of words to find in the puzzle
+    """
     hint_div = WebDriverWait(driver, 2).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, hint_div_class)))
     paragraph = hint_div.find_element(By.TAG_NAME, 'p')
     bold_tags = paragraph.find_elements(By.TAG_NAME, 'b')
@@ -42,12 +56,28 @@ def find_total_words():
         return 0
 
 
-def load_board():
-    board = list()
+def print_board(board: List[List[WebElement]]):
+    """
+    Print the board in a human-readable format
 
-    # I hope these stay cosntant
-    ROWS = 8
-    COLS = 6
+    Args:
+        list(list(WebElement)) board:
+
+    """
+    for row in board:
+        for button in row:
+            print(f'{button.text}', end=' ')
+        print('')
+
+
+def load_board() -> List[List[WebElement]]:
+    """
+    Load the board into an array of buttons that can be clicked through later
+
+    Returns:
+        list(list(WebElement)): Array of buttons
+    """
+    board = list()
     counter = 0
 
     board_div = WebDriverWait(driver, 2).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, board_class)))
@@ -55,14 +85,15 @@ def load_board():
         row = list()
         for x in range(COLS):
             button = board_div.find_element(By.ID, f'button-{counter}')
-            row.append(button.text)
+            row.append(button)
 
             counter += 1
 
         board.append(row)
     
-    print(board)
+    print_board(board)
     return board
+
 
 if __name__ == '__main__':
     start()
